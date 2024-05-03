39c2dd8ca14e61ac28cd32d77b2a63cfad2b7ec0
Giselle <gisellevonbingen@gmail.com>
2024-05-03 11:20:41 +0900

    registerSoundEvents

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/init/ModJobs.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/init/ModJobs.java
@@ -1,36 +1,28 @@
 package steve_gall.minecolonies_compatibility.core.common.init;
 
-import java.util.function.Consumer;
-
 import com.minecolonies.api.colony.jobs.registry.JobEntry;
 import com.minecolonies.core.colony.jobs.views.DefaultJobView;
 
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import steve_gall.minecolonies_compatibility.core.common.MineColoniesCompatibility;
 import steve_gall.minecolonies_compatibility.core.common.colony.job.JobGunner;
 import steve_gall.minecolonies_compatibility.core.common.colony.job.JobOrchardist;
-import steve_gall.minecolonies_tweaks.api.registries.DeferredRegisterHelper;
+import steve_gall.minecolonies_tweaks.api.registries.JobRegister;
 
 public class ModJobs
 {
-	public static final DeferredRegister<JobEntry> REGISTER = DeferredRegisterHelper.jobs(MineColoniesCompatibility.MOD_ID);
+	public static final JobRegister REGISTER = new JobRegister(MineColoniesCompatibility.MOD_ID);
 
-	public static final RegistryObject<JobEntry> GUNNER = register("gunner", builder ->
+	public static final RegistryObject<JobEntry> GUNNER = REGISTER.register("gunner", builder ->
 	{
 		builder.setJobProducer(JobGunner::new);
 		builder.setJobViewProducer(() -> DefaultJobView::new);
 	});
-	public static final RegistryObject<JobEntry> ORCHARDIST = register("orchardist", builder ->
+	public static final RegistryObject<JobEntry> ORCHARDIST = REGISTER.register("orchardist", builder ->
 	{
 		builder.setJobProducer(JobOrchardist::new);
 		builder.setJobViewProducer(() -> DefaultJobView::new);
 	});
-
-	public static RegistryObject<JobEntry> register(String path, Consumer<JobEntry.Builder> consumer)
-	{
-		return DeferredRegisterHelper.registerJobEntry(REGISTER, path, consumer);
-	}
 
 	private ModJobs()
 	{

b3ed376d7c99ca098b46e52dba1bd9e4601447c7
Giselle <gisellevonbingen@gmail.com>
2024-05-04 22:31:06 +0900

    Support polymorph at teaching recipe

---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/mixin/minecolonies/ContainerCraftingMixin.java
@@ -0,0 +1,58 @@
+package steve_gall.minecolonies_compatibility.core.common.mixin.minecolonies;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.minecolonies.api.inventory.container.ContainerCrafting;
+import com.minecolonies.api.util.Tuple;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
+import steve_gall.minecolonies_compatibility.core.common.module.ModuleManager;
+import steve_gall.minecolonies_compatibility.core.common.module.polymorph.PolymorphModule;
+
+@Mixin(value = ContainerCrafting.class, remap = false)
+public abstract class ContainerCraftingMixin extends AbstractContainerMenu
+{
+	@Shadow
+	private Level world;
+	@Shadow
+	private Inventory inv;
+	@Shadow
+	private CraftingContainer craftMatrix;
+	@Shadow
+	private Slot craftResultSlot;
+
+	protected ContainerCraftingMixin(MenuType<?> p_38851_, int p_38852_)
+	{
+		super(p_38851_, p_38852_);
+	}
+
+	@Inject(method = "slotsChanged", at = @At(value = "TAIL"), cancellable = true, remap = true)
+	private void slotsChanged(Container inventoryIn, CallbackInfo ci)
+	{
+		if (!this.world.isClientSide && ModuleManager.POLYMORPH.isLoaded())
+		{
+			var notlimited = !this.world.getGameRules().getBoolean(GameRules.RULE_LIMITED_CRAFTING);
+			var player = (ServerPlayer) this.inv.player;
+			var tuples = this.world.getRecipeManager().getRecipesFor(RecipeType.CRAFTING, this.craftMatrix, this.world).stream().filter(//
+					recipe -> recipe.isSpecial() || (notlimited || player.getRecipeBook().contains(recipe) || player.isCreative())//
+			).map(recipe -> new Tuple<>(recipe, recipe.assemble(this.craftMatrix))).toList();
+			var output = this.craftResultSlot.getItem();
+			PolymorphModule.sendRecipesList(player, tuples, output);
+		}
+
+	}
+
+}
---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/module/polymorph/PolymorphModule.java
@@ -0,0 +1,118 @@
+package steve_gall.minecolonies_compatibility.core.common.module.polymorph;
+
+import java.util.List;
+import java.util.TreeSet;
+
+import com.illusivesoulworks.polymorph.api.PolymorphApi;
+import com.illusivesoulworks.polymorph.api.client.base.IPolymorphClient.IRecipesWidgetFactory;
+import com.illusivesoulworks.polymorph.api.client.base.IRecipesWidget;
+import com.illusivesoulworks.polymorph.api.client.widget.AbstractRecipesWidget;
+import com.illusivesoulworks.polymorph.api.common.base.IPolymorphCommon.IItemStack2RecipeData;
+import com.illusivesoulworks.polymorph.api.common.base.IRecipePair;
+import com.illusivesoulworks.polymorph.api.common.capability.IStackRecipeData;
+import com.illusivesoulworks.polymorph.common.impl.RecipePair;
+import com.minecolonies.api.blocks.ModBlocks;
+import com.minecolonies.api.inventory.container.ContainerCrafting;
+import com.minecolonies.api.util.Tuple;
+import com.minecolonies.core.client.gui.containers.WindowCrafting;
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import steve_gall.minecolonies_compatibility.core.common.MineColoniesCompatibility;
+
+public class PolymorphModule
+{
+	public static void onLoad()
+	{
+		PolymorphApi.common().registerContainer2ItemStack(menu ->
+		{
+			if (menu instanceof ContainerCrafting crafting)
+			{
+				var stack = new ItemStack(ModBlocks.blockHutTownHall);
+				var tag = stack.getOrCreateTagElement(MineColoniesCompatibility.MOD_ID);
+				tag.putBoolean("polymorph", true);
+				tag.putString("player", crafting.getPlayer().getStringUUID());
+				tag.put("building", NbtUtils.writeBlockPos(crafting.getPos()));
+				return stack;
+			}
+			return null;
+
+		});
+		PolymorphApi.common().registerItemStack2RecipeData(new IItemStack2RecipeData()
+		{
+			@Override
+			public IStackRecipeData createRecipeData(ItemStack stack)
+			{
+				var tag = stack.getTagElement(MineColoniesCompatibility.MOD_ID);
+
+				if (tag != null && tag.getBoolean("polymorph"))
+				{
+					return new CraftingWindowRecipeData(stack);
+				}
+
+				return null;
+			}
+		});
+
+		if (FMLEnvironment.dist.isClient())
+		{
+			PolymorphApi.client().registerWidget(new IRecipesWidgetFactory()
+			{
+				@Override
+				public IRecipesWidget createWidget(AbstractContainerScreen<?> screen)
+				{
+					if (screen instanceof WindowCrafting window)
+					{
+						return new AbstractRecipesWidget(screen)
+						{
+							@Override
+							public Slot getOutputSlot()
+							{
+								return this.containerScreen.getMenu().getSlot(0);
+							}
+
+							@Override
+							public void selectRecipe(ResourceLocation id)
+							{
+								PolymorphApi.common().getPacketDistributor().sendStackRecipeSelectionC2S(id);
+							}
+
+						};
+					}
+
+					return null;
+				}
+			});
+
+		}
+
+	}
+
+	public static void sendRecipesList(ServerPlayer player, List<Tuple<CraftingRecipe, ItemStack>> tuples, ItemStack current)
+	{
+		var pairs = new TreeSet<IRecipePair>();
+		ResourceLocation selected = null;
+
+		for (var tuple : tuples)
+		{
+			var id = tuple.getA().getId();
+			var stack = tuple.getB();
+			pairs.add(new RecipePair(id, stack));
+
+			if (ItemStack.matches(current, stack))
+			{
+				selected = id;
+			}
+
+		}
+
+		PolymorphApi.common().getPacketDistributor().sendRecipesListS2C(player, pairs, selected);
+	}
+
+}

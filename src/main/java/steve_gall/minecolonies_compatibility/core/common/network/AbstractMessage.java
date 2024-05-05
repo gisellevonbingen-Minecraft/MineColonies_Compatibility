4b38d3cb5d99187f162e6836d1c4eaf73f80b39e
Giselle <gisellevonbingen@gmail.com>
2024-05-05 11:46:34 +0900

    Added NetworkChannel

---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/network/AbstractMessage.java
@@ -0,0 +1,28 @@
+package steve_gall.minecolonies_compatibility.core.common.network;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+
+public abstract class AbstractMessage
+{
+	public AbstractMessage()
+	{
+
+	}
+
+	public AbstractMessage(FriendlyByteBuf buffer)
+	{
+
+	}
+
+	public void encode(FriendlyByteBuf buffer)
+	{
+
+	}
+
+	public void handle(NetworkEvent.Context context)
+	{
+
+	}
+
+}

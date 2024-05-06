940ee543654061ca711923bd89afb1b75e10a640
Giselle <gisellevonbingen@gmail.com>
2024-05-06 15:41:17 +0900

    Update CropBlockCoFHAccessor.java

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/mixin/thermal/CropBlockCoFHAccessor.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/mixin/thermal/CropBlockCoFHAccessor.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 import cofh.lib.block.CropBlockCoFH;
 import net.minecraft.world.level.ItemLike;
 
-@Mixin(value = CropBlockCoFH.class)
+@Mixin(value = CropBlockCoFH.class, remap = false)
 public interface CropBlockCoFHAccessor
 {
 	@Invoker("getCropItem")

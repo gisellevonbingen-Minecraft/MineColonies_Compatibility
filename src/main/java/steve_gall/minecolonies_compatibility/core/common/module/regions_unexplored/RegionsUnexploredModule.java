77011f769fc37b7e031c647a847db1ee73d0c56e
Giselle <gisellevonbingen@gmail.com>
2024-05-06 22:57:29 +0900

    Support Regions Unexplored

---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/module/regions_unexplored/RegionsUnexploredModule.java
@@ -0,0 +1,13 @@
+package steve_gall.minecolonies_compatibility.core.common.module.regions_unexplored;
+
+import steve_gall.minecolonies_compatibility.api.common.entity.plant.CustomizedFruit;
+
+public class RegionsUnexploredModule
+{
+	public static void onLoad()
+	{
+		CustomizedFruit.register(new AppleLeavesFruit());
+		CustomizedFruit.register(new SalmonBerryFruit());
+	}
+
+}

25d3c561162da91a3f108ffd6e19cc6ea919a593
Giselle <gisellevonbingen@gmail.com>
2024-05-09 00:19:08 +0900

    Support Orebeeries Replanted

---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/module/oreberries/OreberriesModule.java
@@ -0,0 +1,12 @@
+package steve_gall.minecolonies_compatibility.core.common.module.oreberries;
+
+import steve_gall.minecolonies_compatibility.api.common.entity.plant.CustomizedFruit;
+
+public class OreberriesModule
+{
+	public static void onLoad()
+	{
+		CustomizedFruit.register(new OreBerryFruit());
+	}
+
+}

ad6ce88ef6538994303a74b17656129e3c48f407
Giselle <gisellevonbingen@gmail.com>
2024-05-08 19:46:33 +0900

    Fix crash

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/module/ie/IEModule.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/module/ie/IEModule.java
@@ -1,6 +1,8 @@
 package steve_gall.minecolonies_compatibility.core.common.module.ie;
 
 import blusunrize.immersiveengineering.api.tool.BulletHandler;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import steve_gall.minecolonies_compatibility.api.common.entity.ai.CustomizedAI;
 import steve_gall.minecolonies_compatibility.api.common.entity.plant.CustomizedCrop;
 import steve_gall.minecolonies_tweaks.api.common.requestsystem.RequestableObjectRegistry;
@@ -9,10 +11,17 @@ public class IEModule
 {
 	public static void onLoad()
 	{
-		RequestableObjectRegistry.register(Bullet.ID, Bullet::serialize, Bullet::deserialize);
-		BulletHandler.registerBullet(DefaultBullet.ID, DefaultBullet.INSTANCE);
+		var fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
+		fml_bus.addListener(IEModule::onFMLCommonSetup);
+
 		CustomizedAI.register(new GunnerRevolverAI());
 		CustomizedCrop.register(new HempCrop());
+	}
+
+	private static void onFMLCommonSetup(FMLCommonSetupEvent e)
+	{
+		RequestableObjectRegistry.register(Bullet.ID, Bullet::serialize, Bullet::deserialize);
+		BulletHandler.registerBullet(DefaultBullet.ID, DefaultBullet.INSTANCE);
 	}
 
 }

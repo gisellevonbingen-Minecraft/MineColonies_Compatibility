0a6e129af8ba0a3ed0c6a7261d8dc035593abc0e
Giselle <gisellevonbingen@gmail.com>
2024-05-19 04:01:13 +0900

    Fix

--- src/main/java/steve_gall/minecolonies_compatibility/module/common/farmersdelight/entity/ai/FarmersCookConfig.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/farmersdelight/entity/ai/FarmersCookConfig.java
@@ -3,14 +3,14 @@ package steve_gall.minecolonies_compatibility.module.common.farmersdelight.entit
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
 
-public class FamersCookConfig
+public class FarmersCookConfig
 {
 	public final DoubleValue acceleratePerSkillLevel;
 
-	public FamersCookConfig(ForgeConfigSpec.Builder builder)
+	public FarmersCookConfig(ForgeConfigSpec.Builder builder)
 	{
 		builder.comment("Reduce cooking ticsk by (int)(skillevel *  acceleratePerSkillLevel)");
-		this.acceleratePerSkillLevel = builder.defineInRange("acceleratePerSkillLevel", 0.2D, 0.1D, Integer.MAX_VALUE);
+		this.acceleratePerSkillLevel = builder.defineInRange("acceleratePerSkillLevel", 0.2D, 0.0D, Integer.MAX_VALUE);
 	}
 
 }

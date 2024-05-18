4ab9cc90cdfde6800b53b4bad1abf5748539be92
Giselle <gisellevonbingen@gmail.com>
2024-05-19 04:01:08 +0900

    Fix

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/config/JobConfig.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/config/JobConfig.java
@@ -2,12 +2,12 @@ package steve_gall.minecolonies_compatibility.core.common.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import steve_gall.minecolonies_compatibility.core.common.entity.ai.orchardist.OrchardistConfig;
-import steve_gall.minecolonies_compatibility.module.common.farmersdelight.entity.ai.FamersCookConfig;
+import steve_gall.minecolonies_compatibility.module.common.farmersdelight.entity.ai.FarmersCookConfig;
 
 public class JobConfig
 {
 	public final OrchardistConfig orchardist;
-	public final FamersCookConfig farmersCook;
+	public final FarmersCookConfig farmersCook;
 
 	public JobConfig(ForgeConfigSpec.Builder builder)
 	{
@@ -16,7 +16,7 @@ public class JobConfig
 		builder.pop();
 
 		builder.push("farmersCook");
-		this.farmersCook = new FamersCookConfig(builder);
+		this.farmersCook = new FarmersCookConfig(builder);
 		builder.pop();
 	}
 

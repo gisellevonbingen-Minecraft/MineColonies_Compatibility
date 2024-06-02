a4e679f010d3829005545191553e687108f22307
Giselle <gisellevonbingen@gmail.com>
2024-06-02 09:28:41 +0900

    Fixed crash with Regions Unexplored

--- src/main/java/steve_gall/minecolonies_compatibility/module/common/regions_unexplored/RegionsUnexploredModule.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/regions_unexplored/RegionsUnexploredModule.java
@@ -1,13 +1,24 @@
 package steve_gall.minecolonies_compatibility.module.common.regions_unexplored;
 
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import steve_gall.minecolonies_compatibility.api.common.plant.CustomizedFruit;
 
 public class RegionsUnexploredModule
 {
 	public static void onLoad()
 	{
-		CustomizedFruit.register(new AppleLeavesFruit());
-		CustomizedFruit.register(new SalmonBerryFruit());
+		var fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
+		fml_bus.addListener(RegionsUnexploredModule::onFMLCommonSetup);
+	}
+
+	private static void onFMLCommonSetup(FMLCommonSetupEvent e)
+	{
+		e.enqueueWork(() ->
+		{
+			CustomizedFruit.register(new AppleLeavesFruit());
+			CustomizedFruit.register(new SalmonBerryFruit());
+		});
 	}
 
 }

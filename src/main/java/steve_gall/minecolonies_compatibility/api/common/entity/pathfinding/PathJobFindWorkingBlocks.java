ec6ae0d617a2051db69cdff2f40600fed7a4d69e
Giselle <gisellevonbingen@gmail.com>
2024-06-08 20:58:29 +0900

    Update PathJobFindWorkingBlocks.java

--- src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/pathfinding/PathJobFindWorkingBlocks.java
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/pathfinding/PathJobFindWorkingBlocks.java
@@ -3,10 +3,13 @@ package steve_gall.minecolonies_compatibility.api.common.entity.pathfinding;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import com.minecolonies.core.entity.pathfinding.MNode;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import steve_gall.minecolonies_tweaks.api.common.pathfinding.SimplePathJob;
 
@@ -34,6 +37,18 @@ public class PathJobFindWorkingBlocks<RESULT extends WorkingBlocksPathResult> ex
 	protected double computeHeuristic(@NotNull BlockPos pos)
 	{
 		return 0.0D;
+	}
+
+	@Override
+	protected boolean isPassable(BlockPos pos, boolean head, MNode parent)
+	{
+		return true;
+	}
+
+	@Override
+	protected boolean isPassable(@NotNull BlockState block, BlockPos pos, MNode parent, boolean head)
+	{
+		return true;
 	}
 
 	@Override

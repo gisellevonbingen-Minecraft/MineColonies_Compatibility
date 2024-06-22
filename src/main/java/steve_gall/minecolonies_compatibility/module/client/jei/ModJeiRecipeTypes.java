f20705876387cf86e50ea758f7eb5ab0d87492ee
Giselle <gisellevonbingen@gmail.com>
2024-06-22 22:20:46 +0900

    Added JEI Research category

--- src/main/java/steve_gall/minecolonies_compatibility/module/client/jei/ModJeiRecipeTypes.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/client/jei/ModJeiRecipeTypes.java
@@ -2,10 +2,14 @@ package steve_gall.minecolonies_compatibility.module.client.jei;
 
 import mezz.jei.api.recipe.RecipeType;
 import steve_gall.minecolonies_compatibility.api.common.plant.FruitIconCache;
+import steve_gall.minecolonies_compatibility.core.common.MineColoniesCompatibility;
 import steve_gall.minecolonies_compatibility.core.common.init.ModJobs;
+import steve_gall.minecolonies_compatibility.module.client.jei.ResearchCategory.ResearchCache;
 
 public class ModJeiRecipeTypes
 {
+	public static final RecipeType<ResearchCache> RESEARCH = new RecipeType<>(MineColoniesCompatibility.rl("research"), ResearchCache.class);
+
 	public static final RecipeType<FruitIconCache> ORCHARDIST_FRUIT = new RecipeType<>(ModJobs.ORCHARDIST.getId(), FruitIconCache.class);
 
 	private ModJeiRecipeTypes()
--- src/main/java/steve_gall/minecolonies_compatibility/module/client/jei/ModPlugin.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/client/jei/ModPlugin.java
@@ -1,9 +1,14 @@
 package steve_gall.minecolonies_compatibility.module.client.jei;
 
+import java.util.stream.Stream;
+
+import com.minecolonies.api.MinecoloniesAPIProxy;
 import com.minecolonies.api.blocks.ModBlocks;
+import com.minecolonies.api.research.IGlobalResearch;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.resources.ResourceLocation;
@@ -12,6 +17,7 @@ import steve_gall.minecolonies_compatibility.api.common.plant.CustomizedFruit;
 import steve_gall.minecolonies_compatibility.api.common.plant.FruitIconCache;
 import steve_gall.minecolonies_compatibility.core.common.MineColoniesCompatibility;
 import steve_gall.minecolonies_compatibility.core.common.init.ModJobs;
+import steve_gall.minecolonies_compatibility.module.client.jei.ResearchCategory.ResearchCache;
 
 @JeiPlugin
 public class ModPlugin implements IModPlugin
@@ -22,13 +28,34 @@ public class ModPlugin implements IModPlugin
 		var job = ModJobs.ORCHARDIST.get().produceJob(null);
 		var guiHelper = registration.getJeiHelpers().getGuiHelper();
 
+		registration.addRecipeCategories(new ResearchCategory(guiHelper));
 		registration.addRecipeCategories(new OrchardistCategory(job, ModJeiRecipeTypes.ORCHARDIST_FRUIT, new ItemStack(ModBlocks.blockHutLumberjack), guiHelper));
 	}
 
 	@Override
 	public void registerRecipes(IRecipeRegistration registration)
 	{
+		registration.addRecipes(ModJeiRecipeTypes.RESEARCH, this.getGlobalResearches().map(ResearchCache::new).toList());
 		registration.addRecipes(ModJeiRecipeTypes.ORCHARDIST_FRUIT, CustomizedFruit.getRegistry().values().stream().map(FruitIconCache::new).toList());
+	}
+
+	@Override
+	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration)
+	{
+		registration.addRecipeCatalyst(new ItemStack(ModBlocks.blockHutUniversity), ModJeiRecipeTypes.RESEARCH);
+	}
+
+	public Stream<IGlobalResearch> getGlobalResearches()
+	{
+		var api = MinecoloniesAPIProxy.getInstance().getGlobalResearchTree();
+		return api.getBranches().stream().flatMap(branchId -> api.getPrimaryResearch(branchId).stream()).flatMap(this::getGlobalResearches);
+	}
+
+	public Stream<IGlobalResearch> getGlobalResearches(ResourceLocation researchId)
+	{
+		var api = MinecoloniesAPIProxy.getInstance().getGlobalResearchTree();
+		var research = api.getResearch(researchId);
+		return Stream.concat(Stream.of(research), research.getChildren().stream().flatMap(this::getGlobalResearches));
 	}
 
 	@Override

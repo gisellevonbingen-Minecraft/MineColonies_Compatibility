86bb3618eb19de1fe50403f58e3ffbc67b36d700
Giselle <gisellevonbingen@gmail.com>
2025-01-12 16:05:08 +0900

    InjectBuildingSettingsModuleEvent

---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/building/module/InjectBuildingSettingsModuleEvent.java
@@ -0,0 +1,40 @@
+package steve_gall.minecolonies_compatibility.core.common.building.module;
+
+import java.util.List;
+
+import com.minecolonies.api.colony.buildings.IBuilding;
+import com.minecolonies.api.colony.buildings.modules.ISettingsModule;
+import com.minecolonies.api.colony.buildings.modules.settings.ISetting;
+import com.minecolonies.api.colony.buildings.modules.settings.ISettingKey;
+import com.minecolonies.api.colony.buildings.modules.settings.ISettingsModuleView;
+import com.minecolonies.api.colony.buildings.registry.BuildingEntry.ModuleProducer;
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraftforge.eventbus.api.Event;
+
+public class InjectBuildingSettingsModuleEvent extends Event
+{
+	private final IBuilding building;
+
+	public InjectBuildingSettingsModuleEvent(IBuilding building)
+	{
+		this.building = building;
+	}
+
+	public IBuilding getBuilding()
+	{
+		return this.building;
+	}
+
+	public void register(ModuleProducer<? extends ISettingsModule, ? extends ISettingsModuleView> producer, List<Pair<ISettingKey<?>, ISetting<?>>> settings)
+	{
+		var module = this.building.getModule(producer);
+
+		if (module != null)
+		{
+			settings.stream().forEach(pair -> module.with(pair.getFirst(), pair.getSecond()));
+		}
+
+	}
+
+}
--- src/main/java/steve_gall/minecolonies_compatibility/mixin/common/minecolonies/BuildingEntryMixin.java
+++ src/main/java/steve_gall/minecolonies_compatibility/mixin/common/minecolonies/BuildingEntryMixin.java
@@ -1,7 +1,5 @@
 package steve_gall.minecolonies_compatibility.mixin.common.minecolonies;
 
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,18 +7,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.colony.buildings.IBuilding;
-import com.minecolonies.api.colony.buildings.ModBuildings;
-import com.minecolonies.api.colony.buildings.modules.ISettingsModule;
-import com.minecolonies.api.colony.buildings.modules.settings.ISetting;
-import com.minecolonies.api.colony.buildings.modules.settings.ISettingKey;
-import com.minecolonies.api.colony.buildings.modules.settings.ISettingsModuleView;
 import com.minecolonies.api.colony.buildings.registry.BuildingEntry;
-import com.minecolonies.api.colony.buildings.registry.BuildingEntry.ModuleProducer;
-import com.minecolonies.core.colony.buildings.modules.BuildingModules;
-import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.core.BlockPos;
-import steve_gall.minecolonies_compatibility.core.common.init.ModBuildingModules;
+import net.minecraftforge.common.MinecraftForge;
+import steve_gall.minecolonies_compatibility.core.common.building.module.InjectBuildingSettingsModuleEvent;
 
 @Mixin(value = BuildingEntry.class, remap = false)
 public abstract class BuildingEntryMixin
@@ -28,33 +19,7 @@ public abstract class BuildingEntryMixin
 	@Inject(method = "produceBuilding", remap = false, at = @At(value = "TAIL"), cancellable = true)
 	private void produceBuilding(BlockPos position, IColony colony, CallbackInfoReturnable<IBuilding> cir)
 	{
-		var building = cir.getReturnValue();
-		var buildingType = building.getBuildingType();
-
-		if (buildingType == ModBuildings.guardTower.get())
-		{
-			this.injectSettings(building, BuildingModules.GUARD_SETTINGS, ModBuildingModules.GUARD_SETTINGS);
-		}
-		else if (buildingType == ModBuildings.barracksTower.get())
-		{
-			this.injectSettings(building, BuildingModules.GUARD_SETTINGS, ModBuildingModules.GUARD_SETTINGS);
-		}
-		else if (buildingType == ModBuildings.lumberjack.get())
-		{
-			this.injectSettings(building, BuildingModules.FORESTER_SETTINGS, ModBuildingModules.ORCHARDIST_SETTINGS);
-		}
-
-	}
-
-	private <MODULECLASS extends ISettingsModule, VIEWCLASS extends ISettingsModuleView> void injectSettings(IBuilding building, ModuleProducer<MODULECLASS, VIEWCLASS> producer, List<Pair<ISettingKey<?>, ISetting<?>>> settings)
-	{
-		var module = building.getModule(producer);
-
-		if (module != null)
-		{
-			settings.stream().forEach(pair -> module.with(pair.getFirst(), pair.getSecond()));
-		}
-
+		MinecraftForge.EVENT_BUS.post(new InjectBuildingSettingsModuleEvent(cir.getReturnValue()));
 	}
 
 }

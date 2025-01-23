99e28f04b924656271312ccd339dc5c69798380f
Giselle <gisellevonbingen@gmail.com>
2025-01-23 12:29:23 +0900

    SalmonberryCrop

--- src/main/java/steve_gall/minecolonies_compatibility/module/common/delightful/SalmonberryCrop.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/delightful/SalmonberryCrop.java
@@ -1,63 +1,65 @@
 package steve_gall.minecolonies_compatibility.module.common.delightful;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import net.brnbrd.delightful.common.block.DelightfulBlocks;
 import net.brnbrd.delightful.common.block.SalmonberryBushBlock;
 import net.brnbrd.delightful.common.item.DelightfulItems;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
-import steve_gall.minecolonies_compatibility.api.common.plant.CustomizedFruit;
+import steve_gall.minecolonies_compatibility.api.common.plant.CustomizedCrop;
 import steve_gall.minecolonies_compatibility.api.common.plant.HarvesterContext;
 import steve_gall.minecolonies_compatibility.api.common.plant.PlantBlockContext;
+import steve_gall.minecolonies_compatibility.api.common.plant.PlantSeedContext;
 
-public class SalmonberryFruit extends CustomizedFruit
+public class SalmonberryCrop extends CustomizedCrop
 {
 	@Override
-	public @NotNull ResourceLocation getId()
+	public boolean isSeed(@NotNull PlantSeedContext context)
 	{
-		return DelightfulBlocks.SALMONBERRY_BUSH.getId();
+		return context.getSeed().is(DelightfulItems.SALMONBERRY_PIPS.get());
 	}
 
 	@Override
-	public @NotNull List<ItemStack> getBlockIcons()
+	public boolean isCrop(@NotNull PlantBlockContext context)
 	{
-		return Arrays.asList(new ItemStack(DelightfulItems.SALMONBERRY_PIPS.get()));
+		return context.getState().is(DelightfulBlocks.SALMONBERRY_BUSH.get());
 	}
 
 	@Override
-	public @NotNull List<ItemStack> getItemIcons()
+	public @Nullable SpecialHarvestPositionFunction getSpecialHarvestPosition(@NotNull PlantBlockContext context)
 	{
-		return Arrays.asList(new ItemStack(DelightfulItems.SALMONBERRIES.get()));
+		return this::getHarvestPosition;
 	}
 
 	@Override
-	public boolean test(@NotNull PlantBlockContext context)
+	public @Nullable SpecialHarvestMethodFunction getSpecialHarvestMethod(@NotNull PlantBlockContext context)
 	{
-		return context.getState().getBlock() == DelightfulBlocks.SALMONBERRY_BUSH.get();
+		return this::harvest;
 	}
 
-	@Override
-	public boolean canHarvest(@NotNull PlantBlockContext context)
-	{
-		return context.getState().getValue(SalmonberryBushBlock.AGE) > 2;
-	}
-
-	@Override
-	public boolean isMaxHarvest(@NotNull PlantBlockContext context)
+	private @Nullable BlockPos getHarvestPosition(@NotNull PlantBlockContext context)
 	{
 		var state = context.getState();
-		return ((SalmonberryBushBlock) state.getBlock()).isMaxAge(state);
+
+		if (state.getBlock() instanceof SalmonberryBushBlock block && block.isMaxAge(state))
+		{
+			return context.getPosition();
+		}
+		else
+		{
+			return null;
+		}
+
 	}
 
-	@Override
-	public @NotNull List<ItemStack> harvest(@NotNull PlantBlockContext context, @NotNull HarvesterContext harvester)
+	private @NotNull List<ItemStack> harvest(@NotNull PlantBlockContext context, @NotNull HarvesterContext harvester)
 	{
 		if (context.getLevel() instanceof ServerLevel level)
 		{
@@ -74,4 +76,5 @@ public class SalmonberryFruit extends CustomizedFruit
 		}
 
 	}
+
 }

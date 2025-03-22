6b8fa78402a8898f0802b9ce9d75576d9526c74d
Giselle <gisellevonbingen@gmail.com>
2025-03-22 14:11:53 +0900

    NetherWartFruit

---
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/minecraft/NetherWartFruit.java
@@ -0,0 +1,65 @@
+package steve_gall.minecolonies_compatibility.module.common.minecraft;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.NetherWartBlock;
+import net.minecraftforge.registries.ForgeRegistries;
+import steve_gall.minecolonies_compatibility.api.common.plant.CustomizedFruit;
+import steve_gall.minecolonies_compatibility.api.common.plant.HarvesterContext;
+import steve_gall.minecolonies_compatibility.api.common.plant.PlantBlockContext;
+
+public class NetherWartFruit extends CustomizedFruit
+{
+	@Override
+	public @NotNull ResourceLocation getId()
+	{
+		return ForgeRegistries.BLOCKS.getKey(Blocks.NETHER_WART);
+	}
+
+	@Override
+	public @NotNull List<ItemStack> getBlockIcons()
+	{
+		return Arrays.asList(new ItemStack(Items.NETHER_WART));
+	}
+
+	@Override
+	public @NotNull List<ItemStack> getItemIcons()
+	{
+		return Arrays.asList(new ItemStack(Items.NETHER_WART));
+	}
+
+	@Override
+	public boolean test(@NotNull PlantBlockContext context)
+	{
+		return context.getState().getBlock() == Blocks.NETHER_WART;
+	}
+
+	@Override
+	public boolean canHarvest(@NotNull PlantBlockContext context)
+	{
+		return context.getState().getValue(NetherWartBlock.AGE) == NetherWartBlock.MAX_AGE;
+	}
+
+	@Override
+	public boolean isMaxHarvest(@NotNull PlantBlockContext context)
+	{
+		return true;
+	}
+
+	@Override
+	public @NotNull List<ItemStack> harvest(@NotNull PlantBlockContext context, @NotNull HarvesterContext harvester)
+	{
+		var drops = context.getDrops(harvester);
+		var newState = context.getState().setValue(NetherWartBlock.AGE, 0);
+		PlantBlockContext.replant(context, drops, newState);
+		return drops;
+	}
+
+}

3ee69fa15d736e0311f8042773e127f042cc0bb6
Giselle <gisellevonbingen@gmail.com>
2025-04-05 05:59:43 +0900

    Avoid mixin conflict with EasyColony

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/building/module/BucketFillingCraftingModule.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/building/module/BucketFillingCraftingModule.java
@@ -129,18 +129,14 @@ public class BucketFillingCraftingModule extends AbstractCraftingModuleWithExter
 	}
 
 	@Override
-	public void onCrafted(@NotNull AbstractEntityCitizen worker, @NotNull BlockPos workingPos, @NotNull IRecipeStorage recipeStorage, boolean result)
+	public void onCrafted(@NotNull AbstractEntityCitizen worker, @NotNull BlockPos workingPos, @NotNull IRecipeStorage recipeStorage)
 	{
-		if (result)
+		var recipe = toRecipe(recipeStorage);
+
+		if (recipe != null)
 		{
-			var recipe = toRecipe(recipeStorage);
-
-			if (recipe != null)
-			{
-				var level = worker.getLevel();
-				this.drain(level, workingPos, level.getBlockState(workingPos), recipe, false);
-			}
-
+			var level = worker.getLevel();
+			this.drain(level, workingPos, level.getBlockState(workingPos), recipe, false);
 		}
 
 	}

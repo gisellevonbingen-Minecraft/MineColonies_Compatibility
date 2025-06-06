4dd62716d7695655b39f66b7a35126c5bedc82dc
Giselle <gisellevonbingen@gmail.com>
2025-06-06 17:00:53 +0900

    Update EntityAIWorkFarmerMixin.java

--- src/main/java/steve_gall/minecolonies_compatibility/mixin/common/minecolonies/EntityAIWorkFarmerMixin.java
+++ src/main/java/steve_gall/minecolonies_compatibility/mixin/common/minecolonies/EntityAIWorkFarmerMixin.java
@@ -1,7 +1,6 @@
 package steve_gall.minecolonies_compatibility.mixin.common.minecolonies;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;

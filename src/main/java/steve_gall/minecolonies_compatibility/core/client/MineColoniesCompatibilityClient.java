46d13b6a0e2ffabe5a293f46476fb690e7478255
Giselle <gisellevonbingen@gmail.com>
2025-06-29 16:57:31 +0900

    Fix #107

--- src/main/java/steve_gall/minecolonies_compatibility/core/client/MineColoniesCompatibilityClient.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/client/MineColoniesCompatibilityClient.java
@@ -1,7 +1,14 @@
 package steve_gall.minecolonies_compatibility.core.client;
 
+import com.minecolonies.api.MinecoloniesAPIProxy;
+import com.minecolonies.api.sounds.ModSoundEvents;
+
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import steve_gall.minecolonies_compatibility.core.common.MineColoniesCompatibility;
 
 public class MineColoniesCompatibilityClient
 {
@@ -9,6 +16,25 @@ public class MineColoniesCompatibilityClient
 	{
 		var fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
 		var forge_bus = MinecraftForge.EVENT_BUS;
+
+		forge_bus.addListener(this::onPlaySoundEvent);
+	}
+
+	@SubscribeEvent(priority = EventPriority.HIGH)
+	private void onPlaySoundEvent(PlaySoundEvent event)
+	{
+		if (event.getSound() == null)
+		{
+			return;
+		}
+
+		var soundLocation = event.getSound().getLocation();
+
+		if (!MinecoloniesAPIProxy.getInstance().getConfig().getClient().citizenVoices.get() && soundLocation.getNamespace().equals(MineColoniesCompatibility.MOD_ID) && soundLocation.getPath().startsWith(ModSoundEvents.CITIZEN_SOUND_EVENT_PREFIX))
+		{
+			event.setSound(null);
+		}
+
 	}
 
 }

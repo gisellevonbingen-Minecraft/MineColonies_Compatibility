b648e5a40e98aaf25eacd4714dd117ecbd6be2f2
Giselle <gisellevonbingen@gmail.com>
2025-08-30 13:41:28 +0900

    Update CustomizedAIGunner.java

--- src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/ai/guard/CustomizedAIGunner.java
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/ai/guard/CustomizedAIGunner.java
@@ -204,7 +204,9 @@ public abstract class CustomizedAIGunner extends CustomizedAIGuard
 		{
 			if (this.isNeedRequestAmmo(user))
 			{
-				return this.requestAmmo(user, false);
+				var async = bulletMode.canDefault();
+				this.requestAmmo(user, async);
+				return async;
 			}
 
 		}

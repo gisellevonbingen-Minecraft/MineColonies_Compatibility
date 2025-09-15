87beb6ca38d16c10eac0a25ee0a8eda3f65ffac2
Giselle <gisellevonbingen@gmail.com>
2025-09-15 21:17:45 +0900

    Update PolymorphModule.java

--- src/main/java/steve_gall/minecolonies_compatibility/module/common/polymorph/PolymorphModule.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/polymorph/PolymorphModule.java
@@ -109,6 +109,12 @@ public class PolymorphModule extends AbstractModule
 		{
 			var id = tuple.getA().getId();
 			var stack = tuple.getB();
+
+			if (stack.isEmpty())
+			{
+				continue;
+			}
+
 			pairs.add(new RecipePair(id, stack));
 
 			if (ItemStackHelper.equals(current, stack))
@@ -134,7 +140,14 @@ public class PolymorphModule extends AbstractModule
 			for (var i = 0; i < recipes.size(); i++)
 			{
 				var recipe = (Recipe<?>) recipes.get(i);
-				pairs.add(new RecipePair(recipe.getId(), recipe.getResultItem()));
+				var result = recipe.getResultItem();
+
+				if (result.isEmpty())
+				{
+					continue;
+				}
+
+				pairs.add(new RecipePair(recipe.getId(), result));
 
 				if (menu.getRecipeIndex() == i)
 				{

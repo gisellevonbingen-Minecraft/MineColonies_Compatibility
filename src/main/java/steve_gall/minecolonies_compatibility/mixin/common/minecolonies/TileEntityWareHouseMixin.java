e26cc3fabb4eb6234b4b4409880a67ee72656ed3
Giselle <gisellevonbingen@gmail.com>
2025-12-11 23:48:56 +0900

    Split again with limit

--- src/main/java/steve_gall/minecolonies_compatibility/mixin/common/minecolonies/TileEntityWareHouseMixin.java
+++ src/main/java/steve_gall/minecolonies_compatibility/mixin/common/minecolonies/TileEntityWareHouseMixin.java
@@ -1,9 +1,5 @@
 package steve_gall.minecolonies_compatibility.mixin.common.minecolonies;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.minecolonies.api.inventory.InventoryCitizen;
 import com.minecolonies.api.tileentities.AbstractTileEntityWareHouse;
-import com.minecolonies.api.util.Tuple;
 import com.minecolonies.core.tileentities.TileEntityWareHouse;
 
 import net.minecraft.core.BlockPos;
@@ -37,26 +32,6 @@ public abstract class TileEntityWareHouseMixin extends AbstractTileEntityWareHou
 		if (module != null && module.hasMatchingItemStack(itemStack, count, ignoreNBT, ignoreDamage, leftOver))
 		{
 			cir.setReturnValue(true);
-		}
-
-	}
-
-	@Inject(method = "getMatchingItemStacksInWarehouse", remap = false, at = @At("TAIL"), cancellable = true)
-	private void getMatchingItemStacksInWarehouse(Predicate<ItemStack> itemStackSelectionPredicate, CallbackInfoReturnable<List<Tuple<ItemStack, BlockPos>>> cir)
-	{
-		var module = this.getBuilding().getModule(ModBuildingModules.NETWORK_STORAGE);
-
-		if (module != null)
-		{
-			var list = module.getMatchingItemStacks(itemStackSelectionPredicate).toList();
-
-			if (list.size() > 0)
-			{
-				var ret = new ArrayList<>(cir.getReturnValue());
-				ret.addAll(list);
-				cir.setReturnValue(ret);
-			}
-
 		}
 
 	}

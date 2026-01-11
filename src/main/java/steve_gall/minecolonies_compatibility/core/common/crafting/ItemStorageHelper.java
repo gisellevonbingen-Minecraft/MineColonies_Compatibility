2f94824f4c5addb05ca3ca655406debfce5ba7bb
Giselle <gisellevonbingen@gmail.com>
2026-01-11 18:38:27 +0900

    Update ItemStorageHelper.java

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/crafting/ItemStorageHelper.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/crafting/ItemStorageHelper.java
@@ -14,12 +14,12 @@ public class ItemStorageHelper
 {
 	public static List<ItemStack> getCraftingRemainings(List<ItemStorage> list)
 	{
-		return ItemStackHelper.getCraftingRemainings(list.stream().map(ItemStorage::getItemStack).toList());
+		return ItemStackHelper.getCraftingRemainings(list.stream().map(ItemStorageHelper::getAmountedStack).toList());
 	}
 
 	public static List<ItemStack> mapAndFilterNotEmpty(List<ItemStorage> list, Function<ItemStack, ItemStack> func)
 	{
-		return ItemStackHelper.mapAndFilterNotEmpty(list.stream().map(ItemStorage::getItemStack).toList(), func);
+		return ItemStackHelper.mapAndFilterNotEmpty(list.stream().map(ItemStorageHelper::getAmountedStack).toList(), func);
 	}
 
 	public static List<ItemStorage> filterNotEmpty(List<ItemStorage> inputs)
@@ -34,7 +34,12 @@ public class ItemStorageHelper
 
 	public static List<ItemStack> getAmountedStacks(ItemStorage storage)
 	{
-		return Collections.singletonList(storage.getItemStack().copyWithCount(storage.getAmount()));
+		return Collections.singletonList(getAmountedStack(storage));
+	}
+
+	public static ItemStack getAmountedStack(ItemStorage storage)
+	{
+		return storage.getItemStack().copyWithCount(storage.getAmount());
 	}
 
 	public static boolean matches(ItemStorage storage, ItemStack stack, boolean min)

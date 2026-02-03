b022c667476e0ce96fa32166679e4681552ab5a9
Giselle <gisellevonbingen@gmail.com>
2026-02-03 12:49:30 +0900

    ModListUtils

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/util/InteractionMessageHelper.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/util/InteractionMessageHelper.java
@@ -2,7 +2,6 @@ package steve_gall.minecolonies_compatibility.core.common.util;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class InteractionMessageHelper
@@ -17,7 +16,7 @@ public class InteractionMessageHelper
 	public static String getModDisplayName(Block block)
 	{
 		var key = ForgeRegistries.BLOCKS.getKey(block);
-		return ModList.get().getModContainerById(key.getNamespace()).map(c -> c.getModInfo().getDisplayName()).orElse(key.getNamespace());
+		return ModListUtils.getDisplayName(key.getNamespace());
 	}
 
 	public static Component getWorkingBlockNotFound(Block block)
---
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/util/ModListUtils.java
@@ -0,0 +1,24 @@
+package steve_gall.minecolonies_compatibility.core.common.util;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.fml.ModList;
+
+public class ModListUtils
+{
+	public static String getDisplayName(String namespace)
+	{
+		return ModList.get().getModContainerById(namespace).map(f -> f.getModInfo().getDisplayName()).orElse(namespace);
+	}
+
+	public static Component getDisplayNameForTooltip(String namespace)
+	{
+		return Component.literal(getDisplayName(namespace)).withStyle(ChatFormatting.BLUE, ChatFormatting.ITALIC);
+	}
+
+	private ModListUtils()
+	{
+
+	}
+
+}

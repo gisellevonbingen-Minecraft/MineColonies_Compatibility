6afb0119bfdcf2e3cf96d4ce25d4ee55c093625f
Giselle <gisellevonbingen@gmail.com>
2026-02-05 22:05:24 +0900

    IItemGhostSlot.canAccept

--- src/main/java/steve_gall/minecolonies_compatibility/api/common/inventory/IItemGhostSlot.java
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/inventory/IItemGhostSlot.java
@@ -1,6 +1,12 @@
 package steve_gall.minecolonies_compatibility.api.common.inventory;
 
+import net.minecraft.world.item.ItemStack;
+
 public interface IItemGhostSlot
 {
+	default boolean canAccept(ItemStack item)
+	{
+		return true;
+	}
 
 }

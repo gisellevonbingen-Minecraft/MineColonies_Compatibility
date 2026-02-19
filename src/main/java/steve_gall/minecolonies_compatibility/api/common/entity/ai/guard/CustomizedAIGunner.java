c2c338e6d0df2d84fcbe4e809318cf892a1c431e
Giselle <gisellevonbingen@gmail.com>
2026-02-19 23:59:54 +0900

    Update CustomizedAIGunner.java

--- src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/ai/guard/CustomizedAIGunner.java
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/ai/guard/CustomizedAIGunner.java
@@ -140,9 +140,10 @@ public abstract class CustomizedAIGunner extends CustomizedAIGuard
 		}
 		else if (spare)
 		{
+			var ammoInInventory = InventoryUtils.getItemCountInItemHandler(user.getInventoryCitizen(), this.getAmmoPredicate(user));
 			var ammoInBuilding = InventoryUtils.getItemCountInProvider(user.getCitizenData().getWorkBuilding(), this.getAmmoPredicate(user));
 
-			if (ammoInBuilding >= this.getAmmoMinRequestCount(user))
+			if ((ammoInInventory + ammoInBuilding) >= this.getAmmoMinRequestCount(user))
 			{
 				return false;
 			}

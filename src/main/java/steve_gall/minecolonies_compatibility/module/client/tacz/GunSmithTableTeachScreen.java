2e72d17f9e3c1725d28d8ae4cc9effc8ca803592
Giselle <gisellevonbingen@gmail.com>
2026-03-08 21:19:51 +0900

    Refactor TaCZ teach

--- src/main/java/steve_gall/minecolonies_compatibility/module/client/tacz/GunSmithTableTeachScreen.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/client/tacz/GunSmithTableTeachScreen.java
@@ -49,16 +49,4 @@ public class GunSmithTableTeachScreen extends TeachCraftingRecipeScreen<GunSmith
 		return new GunSmithTableRecipeStorage(recipe.id(), input, output);
 	}
 
-	@Override
-	public int getSwitchButtonX()
-	{
-		return GunSmithTableTeachMenu.RESULT_X;
-	}
-
-	@Override
-	public int getSwitchButtonY()
-	{
-		return GunSmithTableTeachMenu.RESULT_Y + SWITCH_HEIGHT;
-	}
-
 }
--- src/main/java/steve_gall/minecolonies_compatibility/module/common/tacz/menu/GunSmithTableTeachMenu.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/tacz/menu/GunSmithTableTeachMenu.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 import com.minecolonies.api.colony.buildings.modules.IBuildingModule;
 import com.minecolonies.api.colony.requestsystem.StandardFactoryController;
 import com.minecolonies.api.crafting.ItemStorage;
-import com.tacz.guns.api.item.IAmmo;
-import com.tacz.guns.api.item.IAttachment;
-import com.tacz.guns.api.item.IGun;
 import com.tacz.guns.crafting.GunSmithTableRecipe;
 import com.tacz.guns.init.ModRecipe;
 
@@ -18,12 +15,10 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.SmithingRecipeInput;
-import steve_gall.minecolonies_compatibility.api.common.inventory.IItemGhostSlot;
 import steve_gall.minecolonies_compatibility.api.common.inventory.IMenuRecipeValidator;
 import steve_gall.minecolonies_compatibility.api.common.inventory.MenuRecipeValidatorRecipe;
 import steve_gall.minecolonies_compatibility.core.common.inventory.ContainerHelper;
@@ -40,14 +35,14 @@ public class GunSmithTableTeachMenu extends TeachRecipeMenu<RecipeHolder<GunSmit
 	public static final int INVENTORY_Y = 84;
 
 	public static final int CRAFTING_SLOTS = 9;
-	public static final int CRAFTING_X = 98;
-	public static final int CRAFTING_Y = 17;
+	public static final int CRAFTING_X = 31;
+	public static final int CRAFTING_Y = 18;
 	public static final int CRAFTING_COLUMNS = 3;
 	public static final int CRAFTING_ROW = 3;
 
-	public static final int RESULT_X = 44;
-	public static final int RESULT_Y = 35;
-	
+	public static final int RESULT_X = 125;
+	public static final int RESULT_Y = 36;
+
 	private SmithingRecipeInput dummyInput;
 
 	public GunSmithTableTeachMenu(int windowId, Inventory inventory, IBuildingModule module)
@@ -65,7 +60,7 @@ public class GunSmithTableTeachMenu extends TeachRecipeMenu<RecipeHolder<GunSmit
 	private void setup()
 	{
 		this.dummyInput = new SmithingRecipeInput(ItemStack.EMPTY, ItemStack.EMPTY, ItemStack.EMPTY);
-		
+
 		this.addInventorySlots(INVENTORY_X, INVENTORY_Y);
 
 		this.inputContainer = new TeachContainer(this, CRAFTING_SLOTS);
@@ -74,18 +69,11 @@ public class GunSmithTableTeachMenu extends TeachRecipeMenu<RecipeHolder<GunSmit
 		{
 			var x = i % CRAFTING_COLUMNS;
 			var y = i / CRAFTING_COLUMNS;
-			this.inputSlots.add(this.addSlot(new TeachInputSlot(this.inputContainer, i, CRAFTING_X + x * SLOT_OFFSET, CRAFTING_Y + y * SLOT_OFFSET)
-			{
-				@Override
-				public boolean canAccept(ItemStack item)
-				{
-					return false;
-				}
-			}));
+			this.inputSlots.add(this.addSlot(new TeachInputSlot(this.inputContainer, i, CRAFTING_X + x * SLOT_OFFSET, CRAFTING_Y + y * SLOT_OFFSET)));
 		}
 
 		this.resultContainer = new TeachContainer(this, 1);
-		this.resultSlots.add(this.addSlot(new ResultSlot(this.resultContainer, 0, RESULT_X, RESULT_Y)));
+		this.resultSlots.add(this.addSlot(new TeachResultSlot(this.resultContainer, 0, RESULT_X, RESULT_Y)));
 	}
 
 	@Override
@@ -164,138 +152,46 @@ public class GunSmithTableTeachMenu extends TeachRecipeMenu<RecipeHolder<GunSmit
 	}
 
 	@Override
-	protected void onSlotsChanged(Container container)
-	{
-		if (container == this.inputContainer)
-		{
-			return;
-		}
-
-		super.onSlotsChanged(container);
-	}
-
-	@Override
-	public void handleSlotClick(Slot slot, ItemStack stack)
-	{
-		if (slot.container == this.inputContainer)
-		{
-			if (this.recipe != null)
-			{
-				var inputs = this.recipe.value().getInputs();
-				var index = this.inputSlots.indexOf(slot);
-
-				if (0 <= index && index < inputs.size())
-				{
-					var input = inputs.get(index);
-
-					if (input.getIngredient().test(stack))
-					{
-						stack = stack.copy();
-						stack.setCount(input.getCount());
-						slot.set(stack);
-					}
-
-				}
-
-			}
-
-			return;
-		}
-		else if (slot.container == this.resultContainer)
-		{
-			var player = this.inventory.player;
-			var recipes = player.level().getRecipeManager().getAllRecipesFor(ModRecipe.GUN_SMITH_TABLE_CRAFTING.get());
-
-			for (var recipe : recipes)
-			{
-				if (this.test(stack, recipe.value()))
-				{
-					this.recipe = recipe;
-					this.onRecipeChanged(this.inventory.player.level().registryAccess(), this.dummyInput);
-					this.refreshRecipes(recipe);
-					break;
-				}
-
-			}
-
-			return;
-		}
-
-		super.handleSlotClick(slot, stack);
-	}
-
-	private boolean test(ItemStack stack, GunSmithTableRecipe recipe)
-	{
-		var result = recipe.getResult().getResult();
-		var item = result.getItem();
-
-		if (item == stack.getItem())
-		{
-			if (item instanceof IGun gun)
-			{
-				return gun.getGunId(stack).equals(gun.getGunId(result));
-			}
-			else if (item instanceof IAttachment attachment)
-			{
-				return attachment.getAttachmentId(stack).equals(attachment.getAttachmentId(result));
-			}
-			else if (item instanceof IAmmo ammo)
-			{
-				return ammo.getAmmoId(stack).equals(ammo.getAmmoId(result));
-			}
-
-		}
-
-		return false;
-	}
-
-	@Override
 	protected void onRecipeChanged(HolderLookup.Provider provider, SmithingRecipeInput recipeInput)
 	{
 		if (this.recipe != null)
 		{
 			this.resultContainer.setItem(0, this.recipe.value().getResultItem(provider));
 
+			var stacks = ItemStackHelper.filterNotEmpty(ContainerHelper.getItemStacks(this.inputContainer));
 			var inputs = this.recipe.value().getInputs();
 
-			for (var i = 0; i < this.inputContainer.getContainerSize(); i++)
-			{
-				if (i < inputs.size())
-				{
-					var stack = this.inputContainer.getItem(i);
-					var input = inputs.get(i);
+			this.inputContainer.clearContent();
 
-					if (!input.getIngredient().test(stack))
+			for (var i = 0; i < inputs.size(); i++)
+			{
+				var input = inputs.get(i);
+				var found = ItemStack.EMPTY;
+
+				for (var stack : stacks)
+				{
+					if (input.getIngredient().test(stack))
 					{
-						var examples = input.getIngredient().getItems();
-						stack = examples.length == 0 ? ItemStack.EMPTY : examples[0].copy();
-						this.inputContainer.setItem(i, stack);
+						found = stack.copy();
+						break;
 					}
 
-					stack.setCount(input.getCount());
-
 				}
-				else
+
+				if (found.isEmpty())
 				{
-					this.inputContainer.setItem(i, ItemStack.EMPTY);
+					var examples = input.getIngredient().getItems();
+					found = examples.length == 0 ? ItemStack.EMPTY : examples[0].copy();
 				}
 
+				found.setCount(input.getCount());
+				this.inputContainer.setItem(i, found);
 			}
 
 		}
 		else
 		{
 			this.resultContainer.setItem(0, ItemStack.EMPTY);
-			this.inputContainer.clearContent();
-		}
-
-	}
-
-	private class ResultSlot extends TeachResultSlot implements IItemGhostSlot
-	{
-		public ResultSlot(Container container, int slot, int x, int y)
-		{
-			super(container, slot, x, y);
 		}
 
 	}

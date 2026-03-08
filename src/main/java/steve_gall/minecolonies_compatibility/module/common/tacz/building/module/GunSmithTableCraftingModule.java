5a261904697d7114b970ded14155d1caa3fecae0
Giselle <gisellevonbingen@gmail.com>
2026-03-08 21:51:00 +0900

    Update GunSmithTableCraftingModule.java

--- src/main/java/steve_gall/minecolonies_compatibility/module/common/tacz/building/module/GunSmithTableCraftingModule.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/tacz/building/module/GunSmithTableCraftingModule.java
@@ -14,7 +14,6 @@ import com.minecolonies.core.colony.buildings.moduleviews.CraftingModuleView;
 import com.tacz.guns.block.AbstractGunSmithTableBlock;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 import steve_gall.minecolonies_compatibility.api.common.building.module.AbstractCraftingModuleWithExternalWorkingBlocks;
@@ -39,12 +38,6 @@ public class GunSmithTableCraftingModule extends AbstractCraftingModuleWithExter
 	public boolean isWorkingBlock(@NotNull LevelReader level, @NotNull BlockPos pos, @NotNull BlockState state)
 	{
 		return level.getBlockState(pos).getBlock() instanceof AbstractGunSmithTableBlock;
-	}
-
-	@Override
-	public boolean canBlockRecipeWorking(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull IRecipeStorage recipeStorage)
-	{
-		return super.canBlockRecipeWorking(level, pos, state, recipeStorage);
 	}
 
 	@Override

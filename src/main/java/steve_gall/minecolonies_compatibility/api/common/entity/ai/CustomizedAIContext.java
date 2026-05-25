ce72fe22fff89b89618c9bb32f56e7ede681f812
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-05-25 16:53:44 +0900

    CustomizedAIContext toolType

--- src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/ai/CustomizedAIContext.java
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/entity/ai/CustomizedAIContext.java
@@ -3,6 +3,7 @@ package steve_gall.minecolonies_compatibility.api.common.entity.ai;
 import org.jetbrains.annotations.NotNull;
 
 import com.minecolonies.api.entity.citizen.AbstractEntityCitizen;
+import com.minecolonies.api.util.constant.IToolType;
 
 import net.minecraft.world.item.ItemStack;
 
@@ -10,11 +11,14 @@ public class CustomizedAIContext
 {
 	@NotNull
 	private final AbstractEntityCitizen user;
+	@NotNull
+	private final IToolType toolType;
 	private final int weaponSlot;
 
-	public CustomizedAIContext(@NotNull AbstractEntityCitizen user, int weaponSlot)
+	public CustomizedAIContext(@NotNull AbstractEntityCitizen user, @NotNull IToolType toolType, int weaponSlot)
 	{
 		this.user = user;
+		this.toolType = toolType;
 		this.weaponSlot = weaponSlot;
 	}
 
@@ -22,6 +26,12 @@ public class CustomizedAIContext
 	public AbstractEntityCitizen getUser()
 	{
 		return this.user;
+	}
+
+	@NotNull
+	public IToolType getToolType()
+	{
+		return this.toolType;
 	}
 
 	public int getWeaponSlot()

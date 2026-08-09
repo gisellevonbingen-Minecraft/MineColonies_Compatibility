033eb86bdccf9f3fbf7b5ee711d23ead14a06855
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-09 15:36:43 +0900

    Update PlantBlockContext.java

--- src/main/java/steve_gall/minecolonies_compatibility/api/common/plant/PlantBlockContext.java
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/plant/PlantBlockContext.java
@@ -73,7 +73,7 @@ public class PlantBlockContext extends PlantPositonContext
 		return this.state;
 	}
 
-	public static void replant(@NotNull PlantBlockContext context, @NotNull List<ItemStack> drops, BlockState replantState)
+	public static boolean replant(@NotNull PlantBlockContext context, @NotNull List<ItemStack> drops, BlockState replantState)
 	{
 		var block = context.getState().getBlock();
 		var canReplant = false;
@@ -102,6 +102,7 @@ public class PlantBlockContext extends PlantPositonContext
 			level.setBlock(context.getPosition(), canReplant ? replantState : Blocks.AIR.defaultBlockState(), Block.UPDATE_CLIENTS);
 		}
 
+		return canReplant;
 	}
 
 }

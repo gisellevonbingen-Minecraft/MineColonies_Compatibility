afa77e1524e5871c96cec57d88a70480942bde7f
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-13 21:55:31 +0900

    knife_dig

--- src/main/java/steve_gall/minecolonies_compatibility/core/common/tool/KnifeToolType.java
+++ src/main/java/steve_gall/minecolonies_compatibility/core/common/tool/KnifeToolType.java
@@ -6,6 +6,7 @@ import com.minecolonies.api.equipment.ModEquipmentTypes;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.ToolAction;
 import steve_gall.minecolonies_tweaks.api.common.tool.CustomToolType;
 
 public class KnifeToolType extends CustomToolType
@@ -19,6 +20,12 @@ public class KnifeToolType extends CustomToolType
 	public int getToolLevel(@NotNull ItemStack stack)
 	{
 		return ModEquipmentTypes.vanillaToolLevel(stack, this.getToolType());
+	}
+
+	@Override
+	public boolean isTool(@NotNull ItemStack stack)
+	{
+		return stack.canPerformAction(ToolAction.get("knife_dig"));
 	}
 
 }
